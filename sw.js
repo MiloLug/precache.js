b850c1a7c2bd9e3dbfffc083da4b5a26dab0f80c
Mikhail Gonchar Yurievich <33329419+MiloLug@users.noreply.github.com>
2018-11-03 22:41:09 +0200

    Create sw.js
    v1 sw.js loaded

---
+++ sw.js
@@ -0,0 +1,56 @@
+self.addEventListener('message', function (e) {
+	var s = e.data;
+	if (!s || s.updateSw) {
+		self.skipWaiting();
+		return;
+	}
+	var type = s.type;
+	s = s.s;
+  	if (type === "install") {
+		caches.open(s.cacheName).then(function (c) {
+          	for(var nm in s.cacheFiles){
+              	s.cacheFiles[nm].forEach(function (f) {
+					c.match(nm+"/"+f).then(function (cR) {
+						if (!cR)
+						c.add(nm+"/"+f);
+					});
+				});
+            }
+		});
+		return;
+	}
+	if (type === "update") {
+		caches.open(s.cacheName).then(function (c) {
+          	var cf=[];
+			for(var nm in s.cacheFiles){
+              	s.cacheFiles[nm].forEach(function (f) {
+					cf.push(c.add(nm+"/"+f));
+				});
+            }
+          	return Promise.all(cf);
+		}).then(function(){
+          	e.source.postMessage({type:"reload"});
+        });
+		return;
+	}
+});
+var fn = function (e) {
+	console.info('[ServiceWorker] Fetch', e.request.url);
+	e.respondWith(
+		caches.match("@:precacheJS_cacheName_").then(function(r){
+          	return r.text();
+        }).then(function(t){
+          	return caches.open(t).then(function (c) {
+              	return c.match(e.request).then(function (response) {
+                  	return response || fetch(e.request).then(function(r){
+						return r;
+                    },function(){
+                      	return new Response("");
+                    });
+				});
+			})
+        })
+    );
+};
+self.removeEventListener('fetch', fn);
+self.addEventListener('fetch', fn);
