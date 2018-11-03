1e2065c2d30e5cd2d6a24073fde9f694bec71d70
Mikhail Gonchar Yurievich <33329419+MiloLug@users.noreply.github.com>
2018-11-03 23:56:51 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,51 +6,48 @@ self.addEventListener('message', function (e) {
 	}
 	var type = s.type;
 	s = s.s;
-  	if (type === "install") {
+	if (type === "install") {
 		caches.open(s.cacheName).then(function (c) {
-          	for(var nm in s.cacheFiles){
-              	s.cacheFiles[nm].forEach(function (f) {
-					c.match(nm+"/"+f).then(function (cR) {
+			for (var nm in s.cacheFiles) {
+				s.cacheFiles[nm].forEach(function (f) {
+					c.match(nm + "/" + f).then(function (cR) {
 						if (!cR)
-						c.add(nm+"/"+f);
+							c.add(nm + "/" + f);
 					});
 				});
-            }
+			}
 		});
 		return;
 	}
 	if (type === "update") {
 		caches.open(s.cacheName).then(function (c) {
-          	var cf=[];
-			for(var nm in s.cacheFiles){
-              	s.cacheFiles[nm].forEach(function (f) {
-					cf.push(c.add(nm+"/"+f));
+			var cf = [];
+			for (var nm in s.cacheFiles) {
+				s.cacheFiles[nm].forEach(function (f) {
+					cf.push(c.add(nm + "/" + f));
 				});
-            }
-          	return Promise.all(cf);
-		}).then(function(){
-          	e.source.postMessage({type:"reload"});
-        });
+			}
+			return Promise.all(cf);
+		}).then(function () {
+			e.source.postMessage({
+				type: "reload"
+			});
+		});
 		return;
 	}
 });
 var fn = function (e) {
 	console.info('[ServiceWorker] Fetch', e.request.url);
 	e.respondWith(
-		caches.match("@:precacheJS_cacheName_").then(function(r){
-          	return r.text();
-        }).then(function(t){
-          	return caches.open(t).then(function (c) {
-              	return c.match(e.request).then(function (response) {
-                  	return response || fetch(e.request).then(function(r){
-						return r;
-                    },function(){
-                      	return new Response("");
-                    });
+		caches.match("@:precacheJS_cacheName_").then(function (r) {
+			return r.text();
+		}).then(function (t) {
+			return caches.open(t).then(function (c) {
+				return c.match(e.request).then(function (response) {
+					return response || fetch(e.request);
 				});
 			})
-        })
-    );
+		}));
 };
 self.removeEventListener('fetch', fn);
 self.addEventListener('fetch', fn);
