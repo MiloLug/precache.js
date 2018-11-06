85852c465462aa1f4f273952f5df528b3bcaa9cd
Mikhail Gonchar Yurievich <33329419+MiloLug@users.noreply.github.com>
2018-11-06 19:15:11 +0200

    Update precache.js

--- precache.js
+++ precache.js
@@ -1,4 +1,7 @@
 (function (W) {
+  	if(!('navigator' in W && 'serviceWorker' in navigator)||!Promise)
+      	return (W.precache=function(){});
+  	
 	var args = function (obj, arg) {
 		if (args !== undefined && !(args instanceof Object))
 			return arg;
@@ -11,6 +14,23 @@
 		}
 		return objNew;
 	},
+	pending = function (success) {
+		return new Promise(function (resolve, reject) {
+			(function interval() {
+				try {
+					var sc = success();
+					if (sc)
+						resolve(sc);
+					else
+						setTimeout(interval, 0);
+				} catch (e) {
+					reject(e);
+				}
+			})();
+		});
+	},
+	worker,
+	options,
 	updateCache = function (reg, s) {
 		reg.active.postMessage({
 			type: "update",
@@ -28,7 +48,7 @@
 			alert("update found! The page will now reload."),
 			W.location.reload());
 	},
-	checker = function (options,reg) {
+	checker = function (options, reg) {
 		var check = false,
 		allChecked = {
 			changedFiles: 0,
@@ -37,7 +57,7 @@
 		s = args({
 				changedFiles: false,
 				minTime: false
-			}, options.updateIf||{}),
+			}, options.updateIf || {}),
 		success = function (resp, file) {
 			var tmp,
 			rclone = resp.clone();
@@ -46,9 +66,9 @@
 				tmp = cache.match(file);
 				return Promise.all([
 						resp.text(),
-						tmp.then(function(tmpFile){
-                          	return tmpFile ? tmpFile.text() : tmpFile;
-                        })
+						tmp.then(function (tmpFile) {
+							return tmpFile ? tmpFile.text() : tmpFile;
+						})
 					]);
 			}).then(function (data) {
 				if (data[0] !== data[1])
@@ -89,18 +109,14 @@
 		}
 		allChecked.minTime = true;
 
-		console.log(44);
-		(function interval() {
-			if (allChecked.changedFiles === true && allChecked.minTime === true) {
-				console.log(99999);
-				if (check)
-					updateCache(reg, options);
-				else
-					installCache(reg, options);
-				return;
-			}
-			setTimeout(interval, 1);
-		})();
+		pending(function () {
+			return allChecked.changedFiles === true && allChecked.minTime === true;
+		}).then(function () {
+			if (check)
+				updateCache(reg, options);
+			else
+				installCache(reg, options);
+		}, console.error);
 	},
 	precFun = function (options, offline) {
 		var s = options;
@@ -116,7 +132,9 @@
 			}).then(function (reg) {
 				console.info("ServiceWorker registration successful with scope: ", reg.scope);
 
-				checker(options,reg);
+				checker(options, reg);
+
+				worker = reg;
 
 				if (!navigator.serviceWorker.controller)
 					return;
@@ -161,46 +179,44 @@
 			});
 		});
 	},
-	prec = function (opt,tempCacheName) {
-		if (('navigator' in W && 'serviceWorker' in navigator) || !s.checkServiceWorkers) {
-			window.addEventListener("load", function () {
-				tempCacheName=tempCacheName||"@:precacheJS_TEMP_CACHE_";
-				new Promise(function (resolve, reject) {
-					if (opt.constructor===String){
-						var precOpt="@:precacheJS_"+opt+"_OPTIONS_";
-						fetch(opt, {
-							cache: "no-cache"
-						}).then(
-							function(resp){
-								caches.open(tempCacheName).then(function(cache){
-									cache.put(precOpt,resp);
-								});
-								return resp.clone();
-							},
-							function(resp){
-								return caches.open(tempCacheName).then(function(cache){
-									return cache.match(precOpt);
-								});
-							}
-						).then(function(resp){
-                          	resolve(resp?resp.json():{});
+	prec = function (opt, tempCacheName) {
+		window.addEventListener("load", function () {
+			tempCacheName = tempCacheName || "@:precacheJS_TEMP_CACHE_";
+			new Promise(function (resolve, reject) {
+				if (opt.constructor === String) {
+					var precOpt = "@:precacheJS_" + opt + "_OPTIONS_";
+					fetch(opt, {
+						cache: "no-cache"
+					}).then(
+						function (resp) {
+						caches.open(tempCacheName).then(function (cache) {
+							cache.put(precOpt, resp);
 						});
-                	}else
-						return opt;
-				}).then(function (s) {
-                  	s = args({
-							cacheFiles: {},
-							cacheName: "precaches",
-							updateIf: false,
-							serviceWorkerFile: "./sw.js",
-							scope: "./",
-							checkServiceWorkers: true
-						}, s);
-					s.tempCacheName=tempCacheName;
-					precFun(s);
-				});
+						return resp.clone();
+					},
+						function (resp) {
+						return caches.open(tempCacheName).then(function (cache) {
+							return cache.match(precOpt);
+						});
+					}).then(function (resp) {
+						resolve(resp ? resp.json() : {});
+					});
+				} else
+					return opt;
+			}).then(function (s) {
+				s = args({
+						cacheFiles: {},
+						cacheName: "precaches",
+						updateIf: false,
+						serviceWorkerFile: "./sw.js",
+						scope: "./",
+						checkServiceWorkers: true
+					}, s);
+				options = s;
+				s.tempCacheName = tempCacheName;
+				precFun(s);
 			});
-		}
+		});
 	};
 	W.precache = prec;
 	W.precache.cache = function (name) {
@@ -215,20 +231,28 @@
 				});
 			},
 			get: function (url) {
-				var c=caches.open(name).then(function (c) {
-					return c.match(url);
-				});
-              	return {
-                  	to:function(type){
-                      	return c.then(function(resp){
-                          	return resp?type?resp[type]():resp:undefined;
-                        });
-                    },
-                  	then:function(fn,errFn){
-                      	return c.then(fn,errFn);
-                    }
-                };
+				var c = caches.open(name).then(function (c) {
+						return c.match(url);
+					});
+				return {
+					to: function (type) {
+						return c.then(function (resp) {
+							return resp ? type ? resp[type]() : resp : undefined;
+						});
+					},
+					then: function (fn, errFn) {
+						return c.then(fn, errFn);
+					}
+				};
 			}
 		};
 	};
+	W.precache.worker = pending(function () {
+			return worker;
+		});
+	W.precache.options = pending(function () {
+			return options;
+		});
+  	W.precache.updateCache=updateCache;
+ 	W.precache.installCache=installCache;
 })(window);
